4e8a4c31c0f1e271871860cca1f8dba3d8f959bf
Joe2443 <up240043@alumnos.upa.edu.mx>
2026-02-15 22:37:18 -0600

    Correcciones3

--- src/Helper.elm
+++ src/Helper.elm
@@ -4,125 +4,124 @@ import Html
 
 
 add2 : Int -> Int -> Int
-add2 num1 num2 =
-    num1 + num2
+add2 int1 int2 =
+    int1 + int2
 
 
 add3 : Float -> Float -> Float -> Float
-add3 num1 num2 num3 =
-    num1 + num2 + num3
+add3 int1 int2 int3 =
+    int1 + int2 + int3
 
 
 calc : Int -> Int -> (Int -> Int -> Int) -> Int
-calc num1 num2 operation =
-    operation num1 num2
+calc int1 int2 operator =
+    operator int1 int2
 
 
 languages : List { name : String, releaseYear : Int, currentVersion : String }
 languages =
-    [ { name = "Elm"
+    [ { name = "elm"
       , releaseYear = 2012
       , currentVersion = "0.19.1"
       }
-    , { name = "Haskell"
-      , releaseYear = 1990
-      , currentVersion = "8.10.7"
+    , { name = "javascript"
+      , releaseYear = 1995
+      , currentVersion = "ECMAScript 2025"
       }
     ]
 
 
 languageNames : List { name : String, releaseYear : Int, currentVersion : String } -> List String
-languageNames languagesList =
-    List.map .name languagesList
+languageNames list =
+    List.map .name list
 
 
-type alias User =
-    { name : String
-    , uType : String
-    }
-
-
-onlyStudents : List User -> List String
-onlyStudents lista =
-    List.map estudiante lista
-
-
-estudiante : { name : String, uType : String } -> String
-estudiante miembro =
-    case miembro.uType of
-        "Student" ->
-            .name miembro
-
-        _ ->
-            ""
-
-
-type alias Videogame =
-    { title : String
-    , releaseYear : Int
-    , available : Bool
-    , downloads : Int
-    , genres : List String
-    }
-
-
-gameinfo : List Videogame
-gameinfo =
-    [ { title = "The Legend of Zelda: Breath of the Wild"
-      , releaseYear = 2017
-      , available = True
-      , downloads = 20000000
-      , genres = [ "Action-adventure", "Open world" ]
+users : List { name : String, uType : String }
+users =
+    [ { name = "Roberto"
+      , uType = "Student"
       }
-    , { title = "Super Mario Odyssey"
-      , releaseYear = 2017
-      , available = True
-      , downloads = 15000000
-      , genres = [ "Platform", "Adventure" ]
+    , { name = "Mitsiu"
+      , uType = "Professor"
       }
     ]
 
 
-getVideogameGenres : List Videogame -> List String
+onlyStudents : List { name : String, uType : String } -> List String
+onlyStudents list =
+    let
+        checkStudent : { name : String, uType : String } -> String
+        checkStudent user =
+            if user.uType == "Student" then
+                user.name
+
+            else
+                ""
+    in
+    List.map checkStudent list
+
+
+type alias Videogame =
+    { title : String, releaseYear : Int, available : Bool, downloads : Int, genres : List String }
+
+
+games : List Videogame
+games =
+    [ { title = "Stardew Valley"
+      , releaseYear = 1956
+      , available = True
+      , downloads = 1655155
+      , genres = [ "Action", "Granja", "Cultivar", "Aldeanos" ]
+      }
+    , { title = "Roblox"
+      , releaseYear = 2005
+      , available = False
+      , downloads = 845125
+      , genres = [ "Action", "Online", "Multijuegos", "Avatar" ]
+      }
+    ]
+
+
+getVideogameGenres : List Videogame -> List (List String)
 getVideogameGenres list =
-    List.concatMap .genres list
+    List.map .genres list
+
+
+
+--HTML
 
 
 type alias Computer =
-    { ram : String
-    , model : String
-    , brand : String
-    , screenSize : String
-    }
+    { ram : String, model : String, brand : String, screenSize : String }
 
 
 myLaptop : Computer
 myLaptop =
-    { ram = "16GB"
-    , model = "XPS 15 9500"
-    , brand = "Dell"
-    , screenSize = "15.6 inches"
+    { ram = "12GB"
+    , model = "IdeaPad 3 "
+    , brand = "Lenovo"
+    , screenSize = "15 pulgadas"
     }
 
 
 main : Html.Html msg
 main =
     Html.div []
-        [ Html.h1 []
-            [ Html.text "My laptop" ]
+        [ Html.h1 [] [ Html.text "My laptop" ]
         , Html.div []
             [ Html.ul []
                 [ Html.li []
-                    [ Html.text ("Ram: " ++ myLaptop.ram) ]
-                , Html.li
-                    []
-                    [ Html.text ("Modelo: " ++ myLaptop.model) ]
-                , Html.li
-                    []
-                    [ Html.text ("Marca: " ++ myLaptop.brand) ]
-                , Html.li
-                    []
-                    [ Html.text ("Pulgadas: " ++ myLaptop.screenSize) ]
+                    [ Html.text ("Ram: " ++ myLaptop.ram)
+                    ]
+                , Html.li []
+                    [ Html.text ("Modelo: " ++ myLaptop.model)
+                    ]
+                , Html.li []
+                    [ Html.text ("Marca: " ++ myLaptop.brand)
+                    ]
+                , Html.li []
+                    [ Html.text ("Pulgadas: " ++ myLaptop.screenSize)
+                    ]
                 ]
             ]
         ]
